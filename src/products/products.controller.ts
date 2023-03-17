018f8d6099e6e687488c777e18921d529a3d1057
Cristian <lopezquekk@gmail.com>
2023-03-16 19:41:05 -0500

    First update

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,4 +1,49 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
+import { Client } from 'pg';
+
+const client = new Client({
+  user: 'root',
+  host: 'localhost',
+  database: 'acheteur_db',
+  password: '123456',
+  port: 5432,
+});
+
+client.connect();
+client.query('SELECT * FROM products', (err, res) => {
+  console.error(err);
+  console.log(res.rows);
+});
 
 @Controller('products')
-export class ProductsController {}
+export class ProductsController {
+  // constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getProductList(
+    @Query('limit') limit: number,
+    @Query('offset') offset = 20,
+  ): string {
+    return `pagination limit: ${limit}, pagination: ${offset}`;
+  }
+
+  @Post()
+  createProduct(@Body() payload: any): any {
+    return {
+      isSuccess: true,
+      payload,
+    };
+  }
+
+  /*
+  @Get()
+  getProductList(@Query() params: any): string {
+    const { limit, offset } = params;
+    return `pagination limit: ${limit}, pagination: ${offset}`;
+  }
+  */
+  @Get(':productId')
+  getProductById(@Param('productId') productId: string): string {
+    return `Product ID: ${productId}`;
+  }
+}
